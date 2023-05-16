90b25583929095c9dbe5c45ab41d8551c491c763
jennifer <gwyjennifer@gmail.com>
2023-05-16 20:21:40 +0800

    add kafka producer

---
+++ kafka-producer-wikimedia/src/main/java/net/jennglebell/springboot/WikimediaChangesHandler.java
@@ -0,0 +1,48 @@
+package net.jennglebell.springboot;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class WikimediaChangesHandler implements EventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesHandler.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private String topic;
+
+    public WikimediaChangesHandler(KafkaTemplate<String, String> kafkaTemplate, String topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+
+    }
+
+    // Whenever there is a new event in wikimedia, this Handler will be triggered.
+    // This onMessage will be called and it will read the new event and send it to the topic using Kafka template.
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        LOGGER.info(String.format("event data -> %s", messageEvent.getData()));
+
+        kafkaTemplate.send(topic, messageEvent.getData());
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+}
---
+++ kafka-producer-wikimedia/src/main/java/net/jennglebell/springboot/WikimediaChangesProducer.java
@@ -0,0 +1,38 @@
+package net.jennglebell.springboot;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class WikimediaChangesProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
+
+    // use Kafka template to send messages to Kafka broker, key and value are of String type
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage() throws InterruptedException {
+        String topic = "wikimedia_recentchange";
+
+        // to read real time stream data from wikimedia, we use event source
+        EventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
+
+        // After 10 mins, the event source should be stopped.
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
