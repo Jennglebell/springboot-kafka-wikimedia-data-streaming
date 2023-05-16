74364a40be29639d1fd1b41e8b68254a88a35756
jennifer <gwyjennifer@gmail.com>
2023-05-16 20:50:16 +0800

    save data into mysql

---
+++ kafka-consumer-database/src/main/java/net/jennglebell/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package net.jennglebell.springboot.repository;
+
+import net.jennglebell.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
